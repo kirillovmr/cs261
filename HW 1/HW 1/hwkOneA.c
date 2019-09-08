0eb5fde075d5a0e274e7334f1178d60a002686d6
Viktor Kirillov <mr.kirillov@icloud.com>
2019-09-07 23:54:51 -0500

    Done using bitwise only

--- HW 1/HW 1/hwkOneA.c
+++ HW 1/HW 1/hwkOneA.c
@@ -10,16 +10,16 @@
 //
 //long int hwkOneA(long int x, int n, int m);
 //long int hwkOneA1(long int x, int n, int m);
+//void runTests(void);
 //
 //int main(int argc, const char * argv[]) {
-//    // insert code here...
-//    printf("%lx\n", hwkOneA(0x0123456789abcdef,3,10));
+//    runTests();
 //    return 0;
 //}
 
 long int hwkOneA(long int x, int n, int m) {
     int n1 = 4*(16-n-1), m1 = 4*(16-m-1);
-    long int x2 = x, q = 0xF;
+    long int x2 = x, q = 0xf;
     
     // Clear n.th byte
     x = x ^ (x & (q<<n1));
@@ -31,7 +31,22 @@ long int hwkOneA(long int x, int n, int m) {
     x = x ^ (x & (q<<m1));
     
     // Put n.th byte onto m.th byte
-    x = x | ((x2 & (q<<n1)) >> n1) << m1;
+    x = x | (((x2 & (q<<n1)) >> n1) & 0xf) << m1;
     
     return x;
 }
+
+//void runTests() {
+//    long int tes[] = {
+//        0x0123456789ABCDEF, 3, 10, 0x012A4567893BCDEF,
+//        0x00DCBA9876543210, 1, 2,  0x0D0CBA9876543210,
+//        0xF000000000000010, 0, 15, 0x000000000000001F
+//    };
+//
+//    for (int i=0; i<sizeof(tes)/sizeof(tes[0])/4; i++) {
+//        long int res = hwkOneA(tes[i*4], (int)tes[i*4+1], (int)tes[i*4+2]);
+//        printf("TEST %d %s: %lx %d %d \n", i+1, tes[i*4+3]==res?"OK":"WRONG", tes[i*4], (int)tes[i*4+1], (int)tes[i*4+2]);
+//        printf("EXP: %lx\n", tes[i*4+3]);
+//        printf("RES: %lx\n\n", res);
+//    }
+//}
--- HW 1/HW 1/main.c
+++ HW 1/HW 1/main.c
@@ -6,40 +6,47 @@
 //  Copyright © 2019 Viktor Kirillov. All rights reserved.
 //
 
-#include <stdio.h>
-
-long int hwkOneA(long int x, int n, int m);
-long int hwkOneA1(long int x, int n, int m);
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("\n%lx\n", hwkOneA(0xF000000000000010,0,15));
-    return 0;
-}
+//#include <stdio.h>
+//
+//long int hwkOneA(long int x, int n, int m);
+//long int hwkOneA1(long int x, int n, int m);
+//void runTests(void);
+//
+//int main(int argc, const char * argv[]) {
+//    runTests();
+//    return 0;
+//}
 
 long int hwkOneA(long int x, int n, int m) {
-    int n1 = 4*(16-n-1), m1 = 4*(16-m);
+    int n1 = 4*(16-n-1), m1 = 4*(16-m-1);
     long int x2 = x, q = 0xf;
-    printf("%lx\n", x);
     
     // Clear n.th byte
     x = x ^ (x & (q<<n1));
     
-    printf("%lx\n", x | ((x & (q<<m1)) >> m1) << n1   );
     // Put m.th byte onto n.th byte
     x = x | ((x & (q<<m1)) >> m1) << n1;
-    
-    printf("%lx\n", x);
     
     // Clear m.th byte
     x = x ^ (x & (q<<m1));
     
-    printf("\n%lx\n", x2);
-    printf("%lx\n",  x | (((x2 & (q<<n1)) >> n1) ^ (q<<n1)>>(n1-4)) << m1  );
-//    printf("%lx\n",     );
-    
     // Put n.th byte onto m.th byte
-    x = x | ((x2 & (q<<n1)) >> n1) << m1;
+    x = x | (((x2 & (q<<n1)) >> n1) & 0xf) << m1;
     
     return x;
 }
+
+//void runTests() {
+//    long int tes[] = {
+//        0x0123456789ABCDEF, 3, 10, 0x012A4567893BCDEF,
+//        0x00DCBA9876543210, 1, 2,  0x0D0CBA9876543210,
+//        0xF000000000000010, 0, 15, 0x000000000000001F
+//    };
+//
+//    for (int i=0; i<sizeof(tes)/sizeof(tes[0])/4; i++) {
+//        long int res = hwkOneA(tes[i*4], (int)tes[i*4+1], (int)tes[i*4+2]);
+//        printf("TEST %d %s: %lx %d %d \n", i+1, tes[i*4+3]==res?"OK":"WRONG", tes[i*4], (int)tes[i*4+1], (int)tes[i*4+2]);
+//        printf("EXP: %lx\n", tes[i*4+3]);
+//        printf("RES: %lx\n\n", res);
+//    }
+//}
